32b51258b811511814c3bf42b3cb4baef07f803c
Ivan Landim <ivanlflj@gmail.com>
2017-02-11 16:55:00 -0200

    Perform tests and choose heuristic

--- 100_tournament.py
+++ 100_tournament.py
@@ -1,17 +1,6 @@
 import os
 
-for j in range(5):
-    with open("data_winratio.txt", "a") as myfile:
-        myfile.write('In the move.\n')
-    for i in range(5):
-        os.system('python tournament_in_the_move.py')
-
-    with open("data_winratio.txt", "a") as myfile:
-        myfile.write('One move ahead.\n')
-    for i in range(5):
-        os.system('python tournament.py')
-
-    with open("data_winratio.txt", "a") as myfile:
-        myfile.write('Two moves ahead.\n')
-    for i in range(5):
-        os.system('python tournament_two_moves.py')
+with open("data_winratio.txt", "a") as myfile:
+    myfile.write('One move ahead, without average.\n')
+for i in range(20):
+    os.system('python tournament.py')
