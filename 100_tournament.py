433f1eebf36d2fd0a725f5c5bad73a707b4cc52f
Ivan Landim <ivanlflj@gmail.com>
2017-02-11 10:06:33 -0200

    Perform three tests

--- 100_tournament.py
+++ 100_tournament.py
@@ -1,16 +1,17 @@
 import os
 
-for i in range(20):
+for j in range(5):
     with open("data_winratio.txt", "a") as myfile:
         myfile.write('In the move.\n')
-    os.system('python tournament_in_the_move.py')
+    for i in range(5):
+        os.system('python tournament_in_the_move.py')
 
-for i in range(20):
     with open("data_winratio.txt", "a") as myfile:
         myfile.write('One move ahead.\n')
-    os.system('python tournament.py')
+    for i in range(5):
+        os.system('python tournament.py')
 
-for i in range(20):
     with open("data_winratio.txt", "a") as myfile:
         myfile.write('Two moves ahead.\n')
-    os.system('python tournament_two_moves.py')
+    for i in range(5):
+        os.system('python tournament_two_moves.py')
